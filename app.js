52b5c263120c1b9b56523900456236f9dddc76b2
Alex Sor Kang <asork42@berkeley.edu>
2015-08-03 15:57:58 -0700

    chatroom

--- app.js
+++ app.js
@@ -21,6 +21,14 @@ app.use(express.static(path.join(__dirname, '/public')));
 require('./routes/index')(app);
 
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
